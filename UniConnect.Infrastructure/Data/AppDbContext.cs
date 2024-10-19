b4b8b1a5aa8beb43c9c869ae6a6314e3178d90cf
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 00:55:20 -0300

    feat: adicionar tabela de usuários e tabela de mensagens ao DbContext

--- UniConnect.Infrastructure/Data/AppDbContext.cs
+++ UniConnect.Infrastructure/Data/AppDbContext.cs
@@ -1,9 +1,13 @@
 using Microsoft.EntityFrameworkCore;
+using UniConnect.Domain.Entities;
 
 namespace UniConnect.Infrastructure.Data;
 
-public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) // Fix the class definition here
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Message> Messages { get; set; }
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
