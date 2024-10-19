36d4ef4949b4c8034467f2edd92257a898df6ab4
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 13:41:17 -0300

    feat: criar DTOs para criação e atualização de eventos

---
+++ UniConnect.Application/DTOs/CreateEventDto.cs
@@ -0,0 +1,6 @@
+namespace UniConnect.Application.DTOs;
+
+public class CreateEventDto
+{
+    public string? Description { get; set; }
+}
---
+++ UniConnect.Application/DTOs/UpdateEventDto.cs
@@ -0,0 +1,7 @@
+namespace UniConnect.Application.DTOs;
+
+public class UpdateEventDto
+{
+    public long Id { get; set; }
+    public string? Description { get; set; }
+}
