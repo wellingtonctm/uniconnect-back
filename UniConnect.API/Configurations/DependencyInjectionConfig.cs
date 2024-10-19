cf2f9b9d59d3911060e6d77f93ea8d1bf944b2b6
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 14:59:49 -0300

    feat: registrar serviços e repositórios de usuários e mensagens

--- UniConnect.API/Configurations/DependencyInjectionConfig.cs
+++ UniConnect.API/Configurations/DependencyInjectionConfig.cs
@@ -10,12 +10,16 @@ public static class DependencyInjectionConfig
     public static WebApplicationBuilder AddRepositoriesConfig(this WebApplicationBuilder builder)
     {
         builder.Services.AddScoped<IEventRepository, EventRepository>();
+        builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddScoped<IMessageRepository, MessageRepository>();
         return builder;
     }
 
     public static WebApplicationBuilder AddServicesConfig(this WebApplicationBuilder builder)
     {
         builder.Services.AddScoped<IEventService, EventService>();
+        builder.Services.AddScoped<IUserService, UserService>();
+        builder.Services.AddScoped<IMessageService, MessageService>();
         return builder;
     }
 }
