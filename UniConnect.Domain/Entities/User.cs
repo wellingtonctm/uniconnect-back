93dfa4f8f5ec2ad90588dbf54feb502a1d4ed0ae
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 01:34:51 -0300

    feat: adicionar coluna enabled, eventId e createdAt na tabela de usuários

--- UniConnect.Domain/Entities/User.cs
+++ UniConnect.Domain/Entities/User.cs
@@ -3,5 +3,9 @@ namespace UniConnect.Domain.Entities;
 public class User
 {
     public required long Id { get; set; }
+    public required long EventId { get; set; }
+    public Event? Event { get; set; }
     public required string Name { get; set; }
+    public required DateTime CreatedAt { get; set; }
+    public required bool Enabled { get; set; }
 }
