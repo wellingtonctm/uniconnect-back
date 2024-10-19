3a68c63f169f44ef3f9a338f6fb09caf55909e18
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 14:48:08 -0300

    feat: criar DTOs para criação e atualização de Menagens

---
+++ UniConnect.Application/DTOs/CreateMessageDto.cs
@@ -0,0 +1,6 @@
+namespace UniConnect.Application.DTOs;
+
+public class CreateMessageDto
+{
+    public string? Content { get; set; }
+}
---
+++ UniConnect.Application/DTOs/UpdateMessageDto.cs
@@ -0,0 +1,7 @@
+namespace UniConnect.Application.DTOs;
+
+public class UpdateMessageDto
+{
+    public long Id { get; set; }
+    public string? Content { get; set; }
+}
