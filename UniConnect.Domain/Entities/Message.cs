d5d65b4ca7d0ff2b927cec9cef8acbbb6d201e1b
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 00:47:16 -0300

    feat: criar modelo de mensagem

---
+++ UniConnect.Domain/Entities/Message.cs
@@ -0,0 +1,10 @@
+namespace UniConnect.Domain.Entities;
+
+public class Message
+{
+    public required long Id { get; set; }
+    public required long UserId { get; set; }
+    public User? User { get; set; }
+    public required string Content { get; set; }
+    public required DateTime SentAt { get; set; }
+}
