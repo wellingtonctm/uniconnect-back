1815e6aacc8441646083d52bc5dfa449c62c70ef
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 10:25:00 -0300

    feat: registrar serviço e repositório de eventos

--- UniConnect.API/Configurations/DependencyInjectionConfig.cs
+++ UniConnect.API/Configurations/DependencyInjectionConfig.cs
@@ -1,16 +1,21 @@
-﻿namespace UniConnect.API.Configurations;
+﻿using UniConnect.Application.Interfaces;
+using UniConnect.Application.Services;
+using UniConnect.Domain.Repositories;
+using UniConnect.Infrastructure.Repositories;
+
+namespace UniConnect.API.Configurations;
 
 public static class DependencyInjectionConfig
 {
     public static WebApplicationBuilder AddRepositoriesConfig(this WebApplicationBuilder builder)
     {
-
+        builder.Services.AddScoped<IEventRepository, EventRepository>();
         return builder;
     }
 
     public static WebApplicationBuilder AddServicesConfig(this WebApplicationBuilder builder)
     {
-
+        builder.Services.AddScoped<IEventService, EventService>();
         return builder;
     }
 }
