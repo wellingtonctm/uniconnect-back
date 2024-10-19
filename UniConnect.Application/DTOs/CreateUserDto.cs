0fcb29845c002fad79af78440af0e5329da64af2
wellingtonctm <w.luiscm@gmail.com>
2024-10-19 14:47:58 -0300

    feat: criar DTOs para criação e atualização de usuarios

---
+++ UniConnect.Application/DTOs/CreateUserDto.cs
@@ -0,0 +1,6 @@
+namespace UniConnect.Application.DTOs;
+
+public class CreateUserDto
+{
+    public string? Name { get; set; }
+}
---
+++ UniConnect.Application/DTOs/UpdateUserDto.cs
@@ -0,0 +1,7 @@
+namespace UniConnect.Application.DTOs;
+
+public class UpdateUserDto
+{
+    public long Id { get; set; }
+    public string? Name { get; set; }
+}
