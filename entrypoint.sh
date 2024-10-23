35d32aa1ac34409d32433ca00aae5c6bdf52ef2c
wellingtonctm <w.luiscm@gmail.com>
2024-10-23 15:24:47 -0300

    refactor: adaptar configuração docker para o railwind

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "Running migrations..."
-dotnet ef database update --project UniConnect.Infrastructure
+dotnet ef database update --startup-project UniConnect.API --project UniConnect.Infrastructure
 
 echo "Starting the application..."
 dotnet UniConnect.API.dll
